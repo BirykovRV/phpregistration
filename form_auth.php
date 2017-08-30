bda926a6fafdb7324747d216251b0404ec5d49f0
Roman Birukov <r.v.birukov@yandex.ru>
2017-08-30 16:54:02 +0300

    Работа по восстановлению пароля

--- form_auth.php
+++ form_auth.php
@@ -62,8 +62,11 @@
                     </tr>
 
                     <tr>
-                        <td colspan="2">
+                        <td>
                             <input type="submit" name="btn_submit_auth" value="Войти" />
+                        </td>
+                        <td>
+                            <a href="reset_password.php">Забыли пароль?</a>
                         </td>
                     </tr>
                 </table>
